eac0dee5b4dda836deee87a18eebfc7e5d1843e2
Pedro Henrique Guimarães <pedrohenriqueguimaraestr@gmail.com>
2026-03-26 22:42:58 +0000

    adicinei a variavel valor

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
-
+int valor = 0;
   // Área para exibição dos dados da cidade
 
 return 0;
