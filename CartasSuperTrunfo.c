319b42dfa67578085be5613e17128456f939e60b
Pedro Henrique Guimarães <pedrohenriqueguimaraestr@gmail.com>
2026-03-26 23:23:39 +0000

    Variáveis principais da carta 1 e 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,22 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+ // CARTA 1 
+char estado1;
+char codigo1[4];
+char cidade1[50];
+int populacao1;
+float pib1;
+int pontosTuristicos1;
 
-  // Área para entrada de dados
-int valor = 0;
-  // Área para exibição dos dados da cidade
+// CARTA 2 
+char estado2;
+char codigo2[4];
+char cidade2[50];
+int populacao2;
+float pib2;
+int pontosTuristicos2;
+
 
 return 0;
 } 
